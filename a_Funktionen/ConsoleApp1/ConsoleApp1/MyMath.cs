d597e3e3bd9d9e3d4bbc5225216778b85ae50d07
ayana0206 <schwizerayana@gmail.com>
2025-08-18 16:13:12 +0200

    Aufgabe Funktion 1 - 7 gelöst Class MyMath erstellt + ...

---
+++ a_Funktionen/ConsoleApp1/ConsoleApp1/MyMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MyMath
+    {
+        
+        public static int Calc_kgV(int a, int b)
+        {
+            int z = 0;
+            z = a * b / Calc_ggT(a,b);
+            return z;
+        }
+        public static int Calc_ggT(int a, int b)
+        {
+            int z = 0;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+        public static int Calc_ggT_r(int a, int b)
+        {
+            if (b == 0)
+            { 
+                return a; 
+            }
+            else
+            {
+                return Calc_ggT_r(b, a%b);
+            }                           
+        }
+    }
+}
---
+++ a_Funktionen/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int zahl = MyMath.Calc_ggT(12, 16);
+            Console.WriteLine(zahl); 
+            zahl = MyMath.Calc_kgV(12, 16);
+            Console.WriteLine(zahl);
+            Console.WriteLine(ShowResult("ggT", 12, 16, MyMath.Calc_ggT(12, 16)));
+
+        }
+
+        private static void ShowResult(string s, int a, int b, int r)
+        {
+            Console.WriteLine($"{(s == "ggT"?"ggT":"kgV")} von {a} und {b} ist {r}");
+        }
+        private static int ReadInt()
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Bitte Zahl eingeben");
+                string s = Console.ReadLine();
+                int.TryParse(s, out i);
+            } while (i <= 0);
+            Console.WriteLine("danke");
+            return i;
+        }
+    }
+}
