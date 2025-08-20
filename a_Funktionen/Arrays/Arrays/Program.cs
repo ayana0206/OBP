0931b8cb4d41810a9ebeef262182a26e31a02251
ayana0206 <schwizerayana@gmail.com>
2025-08-20 09:12:53 +0200

    array erstellt

---
+++ a_Funktionen/Arrays/Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[4];
+
+            int[] array1 = { 1, 2, 3 };
+
+            Console.WriteLine(array.Length);
+            Console.WriteLine(array1.Length);
+
+            string[] array2 = { "Hallo", "Hi", "Bonjour" };
+            Console.WriteLine(array2.Length);
+            Console.WriteLine(array2[1]);        
+        }
+    }
+}
