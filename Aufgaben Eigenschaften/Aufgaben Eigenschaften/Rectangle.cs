52fae720719e631599b62dd756f858066ba8fb6f
ayana0206 <schwizerayana@gmail.com>
2025-08-20 15:53:31 +0200

    classen inhalt erstellt

---
+++ Aufgaben Eigenschaften/Aufgaben Eigenschaften/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgaben_Eigenschaften
+{
+    internal class Rectangle
+    {
+        // Eine selber implementierte, read-only Eigenschaft
+        private string name;
+        public string Name 
+        { 
+            get 
+            { 
+                return name; 
+            } 
+        }
+
+        // Eine selber implementierte, read-write Eigenschaft.
+        private int height;
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+        // Eine selber implementierte, write-only Eigenschaft.
+        private int width;
+        public int Width
+        {
+            set
+            {
+                width = value;
+            }
+        }
+        // Eine automatisch implementierte, read-only Eigenschaft.
+        public int Nickname { get; }
+
+        // Eine automatisch implementierte, read-write Eigenschaft.
+        
+        // Eine automatisch implementierte, write-only Eigenschaft.
+
+    }
+}
