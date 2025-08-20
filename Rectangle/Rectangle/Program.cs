dea0bff3111a7f6ba2b068433ed69cb2cbebb079
ayana0206 <schwizerayana@gmail.com>
2025-08-20 15:04:17 +0200

    class rectangle erstellt und properties und fields hinzugefügt

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle();
+
+            // width
+            int width = rectangle1.getWidth();
+            Console.WriteLine(width);
+            
+            // hight
+            rectangle1.setHight(44);
+            int hight = rectangle1. 
+            Console.WriteLine(hight_rec);
+            
+        }
+    }
+}
---
+++ Rectangle/Rectangle/Rectangle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Rectangle
+    {
+        private readonly string name = "Anna";
+        // private int width = 33;
+        // private int hight;
+        private Color color = Color.Aquamarine;
+        private Color fillcolor;
+
+
+        // optimal 
+        public int Width { get; set; }
+        public int Height { get; private set; }
+
+
+        public int Area
+        {
+            get
+            {
+                return Width * Height; // in UML mit / schreiben da 
+            }
+        }
+        // gleich wie 
+        // public int Area => Width * Height
+
+
+        /*public int getWidth()
+        {
+            return this.width;
+        }
+        public int getHight()
+        {
+            return this.hight;
+        }*/
+
+        
+
+        /* ->  beim Setter einfach verändern muss nichts zurückgeben !!
+         
+        public void setWidth(int width)
+        {
+           this.width = width;
+        }
+        public void setHight(int hight)
+        {
+           this.hight = hight;
+        }*/ 
+
+
+    }
+}
