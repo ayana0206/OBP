bb8e3dbe9af43522dbdf4d8dbd7b7bb69c2959db
ayana0206 <schwizerayana@gmail.com>
2025-09-14 17:46:02 +0200

    neues programm erstellt und das für die präsi

---
+++ Portfolio Präsentation Auto/Portfolio Präsentation Auto/Auto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio_Präsentation_Auto
+{
+    internal class Auto
+    {
+        // Fields
+        private string marke;
+        private int sitzplaetze;
+
+        // Properties
+        public int BenzinStand { get; set; }
+
+        // Constructor
+        public Auto(string marke, int sitzplaetze ) 
+        {
+            this.marke = marke;
+            this.sitzplaetze = sitzplaetze;
+        }
+
+        // Methoden
+        public void Fahren(int geschwindigkeit)
+        {
+            if (BenzinStand > 0) 
+            {
+                BenzinStand = BenzinStand - 1;
+                Console.WriteLine($"Ich fahre mit {geschwindigkeit} km/h.");
+            }
+            else
+            {
+                Console.WriteLine("Das Benzin reicht nicht");
+            }
+        }
+        public void KofferraumOeffnen() 
+        {
+            Console.WriteLine("Der Kofferraum ist offen.");
+        }
+    }
+}
---
+++ Portfolio Präsentation Auto/Portfolio Präsentation Auto/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio_Präsentation_Auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto1 = new Auto("Audi", 4);
+            auto1.KofferraumOeffnen(); 
+        }
+    }
+}
