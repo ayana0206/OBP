ba77434450ad487b06534f9de5afd231a462bea6
ayana0206 <schwizerayana@gmail.com>
2025-11-03 20:29:05 +0100

    algorithmen

---
+++ BubbleSort/BubbleSort/BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class BubbleSort
+    {
+        private IList<int> listToSort;
+        public BubbleSort(IList<int> listToSort) 
+        {
+            this.listToSort = listToSort;
+        }
+        public IList<int> Sort()
+        {
+            Boolean swaped = true;
+
+            int length = listToSort.Count;
+            while (swaped == true)
+            {
+                swaped = false;
+                for (var i = 0; i < length-1; i++)
+                {              
+                    if (listToSort[i] > listToSort[i + 1])
+                    {
+                        var temp = listToSort[i];
+                        listToSort[i] = listToSort[i + 1];
+                        listToSort[i + 1] = temp;
+                        swaped = true;
+                    }
+                }
+                length--;
+            }
+            return listToSort;
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> list = [ 4, 2, 7, 10, 5, 1, 5, 2, 0];
+
+            var sorter = new BubbleSort(list);
+
+            foreach (int i in sorter.Sort())
+            {
+                Console.WriteLine(i);
+            }
+
+            
+
+        }
+    }
+}
